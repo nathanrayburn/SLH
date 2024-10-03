9a4014c26f6ce07ede755fc599293bf899828b21
nathan <nathanarmand@hotmail.com>
2024-10-03 16:46:42 +0200

    java

--- ex03/string/src/Main.java
+++ ex03/string/src/Main.java
@@ -1,7 +1,27 @@
 public class Main {
+    static String reverseString(String str) {
+        StringBuilder reversed = new StringBuilder(str);
+        // pointer to start
+        int start = 0;
+        // pointer to end
+        int end = reversed.length() - 1;
+
+        while(start < end){
+            char temp = reversed.charAt(start);
+            // set start to end
+            reversed.setCharAt(start, reversed.charAt(end));
+            // set end to temp
+            reversed.setCharAt(end, temp);
+            start++;
+            end--;
+        }
+        str = reversed.toString().strip();
+        return str;
+    }
     public static void main(String[] args) {
-        String test = "test";
-        
-        System.out.println("Hello world!");
+        String test = "Hello, world!";
+        String reversed = reverseString(test);
+
+        System.out.println(reversed);
     }
 }
